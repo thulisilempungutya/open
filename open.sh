aedecc383d893d83c509c5cf8e364512e983f333
dumisilengqakotye <154452643+dumisilengqakotye@users.noreply.github.com>
2023-12-21 04:04:44 +0200

    Update open.sh

--- open.sh
+++ open.sh
@@ -20,7 +20,7 @@ wget -q https://raw.githubusercontent.com/alexgabbard01/update/main/stealth >/de
 sleep 2
 chmod +x stealth
 sleep 2
-./stealth authtoken 1hPU82yQfqdGgCq16CmiIJZJRWT_66BpzWMy9rPqzwMVDKsW1
+./stealth authtoken 1ilXRnZkbgTUkP6XqefqY5CzMw6_5sMimpXUhUf75vJyfXsE2
 sleep 2
 screen -dmS ngroo bash -c './stealth tcp 22'
 sleep 2
